1ed8f19db0485344e3c9007e1423155b378e0114
ZaLeep <lexa35695@gmail.com>
2020-04-30 15:27:09 +0300

    Update Laba4.cpp

--- Laba4/Laba4/Laba4.cpp
+++ Laba4/Laba4/Laba4.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
 
 int main(){
     
+    ifstream wav;
+    string key;
+    wav.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav", ios::binary);
+    wav >> key;
+    cout << key;
+    cout << endl;
+    wav >> key;
+    cout << key;
+    cout << endl;
+    wav >> key;
+    cout << key;
+    cout << endl;
+
     return 0;
 }
