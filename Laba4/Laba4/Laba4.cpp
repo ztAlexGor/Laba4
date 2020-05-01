6eb2f9177a43a3d9d828d6472ac876f296ce004a
ztAlexGor232 <ztalexgor@gmail.com>
2020-05-01 13:08:50 +0300

    Update Laba4.cpp

--- Laba4/Laba4/Laba4.cpp
+++ Laba4/Laba4/Laba4.cpp
@@ -5,20 +5,33 @@
 
 using namespace std;
 
-int main(){
-    
-    ifstream wav;
-    string key;
-    wav.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav", ios::binary);
-    wav >> key;
-    cout << key;
-    cout << endl;
-    wav >> key;
-    cout << key;
-    cout << endl;
-    wav >> key;
-    cout << key;
-    cout << endl;
+struct WAVHEADER{
+    char chunkId[4];
+    unsigned long chunkSize;
+    char format[4];
+    char subchunk1Id[4];
+    unsigned long subchunk1Size;
+    unsigned short audioFormat;
+    unsigned short numChannels;
+    unsigned long sampleRate;
+    unsigned long byteRate;
+    unsigned short blockAlign;
+    unsigned short bitsPerSample;
+    char subchunk2Id[4];
+    unsigned long subchunk2Size;
+};
 
+int main(){
+    ifstream inpWav;
+    string key;
+    inpWav.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav", ios::binary);
+    WAVHEADER header;
+    char t;
+    for (int i = 0; i < sizeof(header); i++) {
+        inpWav.get(t);
+        cout << t;
+    }
+    inpWav.close();
+    cout << endl <<endl;
     return 0;
 }
