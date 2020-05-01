4350fcad30795ce74b81433c4cdbd8ebaaa7bdca
ZaLeep <lexa35695@gmail.com>
2020-05-01 13:07:21 +0300

    Update Laba4.cpp

--- Laba4/Laba4/Laba4.cpp
+++ Laba4/Laba4/Laba4.cpp
@@ -1,24 +1,38 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include <iomanip>
+#include <cmath>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+class WavHeader {
+    char chunkId[4];
+    unsigned long chunkSize;
+    char format[4];
+    char subchunk1Id[4];
+    unsigned long subchunk1Size;
+    unsigned short audioFormat;
+    unsigned short numChannels;
+    unsigned long sampleRate;
+    unsigned long byteRate;
+    unsigned short blockAlign;
+    unsigned short bitsPerSample;
+    char subchunk2Id[4];
+    unsigned long subchunk2Size;
+public:
+    void ReadHuy(string address) {
+        ifstream take;
+        take.open("D:\\Учёба\\Файлы общего доступа\\test_1.wav");
+        take >> chunkId;
+        cout << chunkId;
+        take >> chunkSize;
+        cout << chunkSize;
+    }
     
-    ifstream wav;
-    string key;
-    wav.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav", ios::binary);
-    wav >> key;
-    cout << key;
-    cout << endl;
-    wav >> key;
-    cout << key;
-    cout << endl;
-    wav >> key;
-    cout << key;
-    cout << endl;
+};
 
-    return 0;
+
+int main() {
+    WavHeader Test_1;
+    Test_1.ReadHuy("D:\\Учёба\\Файлы общего доступа\\test_1.wav");
 }
