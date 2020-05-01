3b87c3632361781d2b62c5895742cc18c043f986
ztAlexGor232 <ztalexgor@gmail.com>
2020-05-01 14:10:13 +0300

    Update Laba4.cpp

--- Laba4/Laba4/Laba4.cpp
+++ Laba4/Laba4/Laba4.cpp
@@ -23,10 +23,23 @@ public:
     void ReadHuy(string address) {
         ifstream take;
         take.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav");
-        take >> chunkId;
-        cout << chunkId;
-        take >> chunkSize;
-        cout << chunkSize;
+
+        take.read(chunkId, sizeof(chunkId));
+        take.read((char*)&chunkSize, sizeof(chunkSize));
+        take.read(format, sizeof(format));
+        take.read(subchunk1Id, sizeof(subchunk1Id));
+        take.read((char*)&subchunk1Size, sizeof(subchunk1Size));
+        take.read((char*)&audioFormat, sizeof(audioFormat));
+        take.read((char*)&numChannels, sizeof(numChannels));
+        take.read((char*)&sampleRate, sizeof(sampleRate));
+        take.read((char*)&byteRate, sizeof(byteRate));
+        take.read((char*)&blockAlign, sizeof(blockAlign));
+        take.read((char*)&bitsPerSample, sizeof(bitsPerSample));
+        take.read(subchunk2Id, sizeof(subchunk2Id));
+        take.read((char*)&subchunk2Size, sizeof(subchunk2Size));
+
+        cout << chunkId << endl << chunkSize << endl << format << endl << subchunk1Id << endl << subchunk1Size << endl << audioFormat << endl << numChannels << endl << sampleRate << endl << byteRate << endl << blockAlign <<  endl << bitsPerSample << endl << subchunk2Id << endl << subchunk2Size <<endl;
+
     }
     
 };
