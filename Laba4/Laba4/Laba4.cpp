716f80dd0146025b7440b0d32e7139773966b11e
ztAlexGor232 <ztalexgor@gmail.com>
2020-05-01 13:09:49 +0300

    Update Laba4.cpp

--- Laba4/Laba4/Laba4.cpp
+++ Laba4/Laba4/Laba4.cpp
@@ -1,11 +1,15 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include <iomanip>
+#include <cmath>
+#include <fstream>
 
 using namespace std;
 
+<<<<<<< HEAD
 struct WAVHEADER{
+=======
+class WavHeader {
+>>>>>>> c044c1893b42374b70a373b573860acff14f3a09
     char chunkId[4];
     unsigned long chunkSize;
     char format[4];
@@ -19,6 +23,7 @@ struct WAVHEADER{
     unsigned short bitsPerSample;
     char subchunk2Id[4];
     unsigned long subchunk2Size;
+<<<<<<< HEAD
 };
 
 int main(){
@@ -34,4 +39,22 @@ int main(){
     inpWav.close();
     cout << endl <<endl;
     return 0;
+=======
+public:
+    void ReadHuy(string address) {
+        ifstream take;
+        take.open("D:\\Учёба\\Файлы общего доступа\\test_1.wav");
+        take >> chunkId;
+        cout << chunkId;
+        take >> chunkSize;
+        cout << chunkSize;
+    }
+    
+};
+
+
+int main() {
+    WavHeader Test_1;
+    Test_1.ReadHuy("D:\\Учёба\\Файлы общего доступа\\test_1.wav");
+>>>>>>> c044c1893b42374b70a373b573860acff14f3a09
 }
