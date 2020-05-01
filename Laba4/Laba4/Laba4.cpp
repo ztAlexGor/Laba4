a23af81159d1ace748789c2f1c96856361a1e256
ztAlexGor232 <ztalexgor@gmail.com>
2020-05-01 21:56:46 +0300

    Update Laba4.cpp

--- Laba4/Laba4/Laba4.cpp
+++ Laba4/Laba4/Laba4.cpp
@@ -23,7 +23,7 @@ public:
     unsigned short first_sample;
     void ReadHuy(string address) {
         ifstream take;
-        take.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav", ios::binary);
+        take.open("D:\\Учёба\\Файлы общего доступа\\Baila Maria_big.wav", ios::binary);
 
         take.read(chunkId, sizeof(chunkId));
         take.read((char*)&chunkSize, sizeof(chunkSize));
@@ -62,7 +62,7 @@ public:
     void Creating() {
 
         ofstream newFile;
-        newFile.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va_2.wav", ios::binary);
+        newFile.open("D:\\Учёба\\Файлы общего доступа\\Baila Maria_big2.wav", ios::binary);
         newFile.write(h.chunkId, sizeof(h.chunkId));
         newFile.write((char*)&h.chunkSize, sizeof(h.chunkSize));
         newFile.write(h.format, sizeof(h.format));
@@ -78,38 +78,37 @@ public:
         newFile.write((char*)&h.subchunk2Size, sizeof(h.subchunk2Size));
 
         ifstream newTake;
-        newTake.open("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav", ios::binary);
-        unsigned long prev = 0, kol = 0;
+        newTake.open("D:\\Учёба\\Файлы общего доступа\\Baila Maria_big.wav", ios::binary);
+        unsigned long prev = -1, newInd;
         unsigned short curr_value = 0, prev_value = 0;
         for (unsigned long curr = 0; curr < size_old / (h.bitsPerSample / 8); curr ++) {
-            kol++;
             newTake.seekg(44 + curr* (h.bitsPerSample / 8));
             newTake.read((char*)&curr_value, sizeof(curr_value));
 
-            unsigned long newInd = curr * coef;
-            if (prev == 0 && curr == 0) {
+            newInd = curr * coef;
+            if (newInd == prev + 1) {//koef == 1
                 newFile.write((char*)&curr_value, sizeof(curr_value));
-            }
-            else if (newInd == prev + 1) {//koef == 1
-                newFile.write((char*)&curr_value, sizeof(curr_value));
+                //cout << 0 << endl;
             }
             else if (newInd == prev) {
-                float function_value;
-                function_value = prev_value + ((curr_value - prev_value) / (curr - prev)) * (curr * coef - prev);
-                newFile.seekp(44 + prev * (h.bitsPerSample / 8));
-                newFile.write((char*)&function_value, sizeof(function_value));
+                //cout << -1 << endl;
             }
             else if (newInd > prev) {
-
+                int temp = prev;
+                int function_value;
+                newFile.seekp(44 + temp * (h.bitsPerSample / 8));
+                while(newInd != temp) {
+                    function_value = prev_value + ((curr_value - prev_value) / (newInd - prev)) * (temp - prev);
+                    newFile.write((char*)&function_value, sizeof(function_value));
+                    temp++; 
+                }
+                //cout << function_value << endl;
             }
-
             prev = newInd;
             prev_value = curr_value;
         }
         newTake.close();
         newFile.close();
-
-        cout << endl << "KOL " << kol << endl;
     }
 };
 
@@ -118,7 +117,7 @@ public:
 
 int main() {
     WavHeader Test_1;
-    Test_1.ReadHuy("D:\\Учёба\\Файлы общего доступа\\Tempo Se Ne Va.wav");
+    Test_1.ReadHuy("D:\\Учёба\\Файлы общего доступа\\Baila Maria_big.wav");
     Test_1.ShowHeader();
     float coef;
     cout << "Enter coefficient of expanding: ";
